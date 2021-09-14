5711042c85945b079df1cd26195cf8ab408e69d1
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2021-09-14 02:54:37 -0700

    This should be pretty close to final.

--- release.sh
+++ release.sh
@@ -2,67 +2,73 @@
 # SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
 # SPDX-License-Identifier: Apache-2.0
 
-function how_to_use {
-    echo "Usage: $0 -v version -r repo [-m] [-p provides]"
-    echo "	-v version      Example: (v1.2.3)"
-    echo "	-r github repo  Example: (octocat/Hello-World)"
-    echo "	-m if passed in generates a meson wrap file"
-    echo "	-p the meson library provided or the project name by default Example: foo -> libfoo = libfoo_dep"
-}
-
-version="none"
-repo_slug="none"
-provides="none"
+version=${GITHUB_REF#refs/tags/}
 meson=0
 
-gh_slug=$1
+repo_slug=$1
 changelog=$2
 meson_wrap=$3
-meson_lib_provided=$4
+meson_provides=$4
+verbose=0
 
-repo_tmp=(${IN//\// })
+if [ "$#" -eq 5 ]; then
+    verbose=$5
+fi
+
+repo_tmp=(${repo_slug//\// })
+
 org=${repo_tmp[0]}
-repo=${repo_tmp[1]}
+repo_name=${repo_tmp[1]}
 
-echo "           gh_slug: $gh_slug"
-echo "              repo: $repo"
-echo "               org: $org"
-echo "            branch: ${GITHUB_REF#refs/tags/}"
-echo "         changelog: $changelog"
-echo "        meson_wrap: $meson_wrap"
-echo "meson_lib_provided: $meson_lib_provided"
-echo "               env:"
-env
-
-ls -la
-
-exit 1
-
-if [ "none" == $version -o "none" == $repo_slug ]; then
-    how_to_use
+# Check the version to make sure we are running on a tag
+raw_version=$version
+if [[ "$version" =~ ^v?[0-9]+\.[0-9]+\.[0-9]+.*$ ]]; then
+    if [ "v" == ${version:0:1} ]; then
+        raw_version=${version:1}
+    fi
+else
+    echo -e "\e[1;31mThis action only works on a version tag.  Examples: v1.0.2, 2.3.0, v99.2.3-release-12\e[0m"
+    echo -e "Branch/tag executed on: \e[1;31m$version\e[0m"
     exit 1
 fi
 
-raw_version=$version
-if [ "v" == ${version:0:1} ]; then
-    raw_version=${version:1}
+# Check for changelog file
+if [[ -f $changelog ]]; then
+    :
+else
+    echo -e "\e[1;31mThe changelog file ($changelog) is not present.\e[0m"
+    exit 1
 fi
 
-repo_name=$(echo $repo_slug | awk -F '/' '{print $2}')
+# Input validate the meson_wrap
+if [[ "$meson_wrap" =~ "true" ]]; then
+    meson=1
+elif [[ "$meson_wrap" =~ "false" ]]; then
+    meson=0
+else
+    echo -e "\e[1;31mmeson-wrap must be true or false.\e[0m"
+    exit 1
+fi
+
 release_slug=$(echo "$repo_name-$raw_version")
 
-if [ "none" == $provides ]; then
-    provides=$repo_name
+if [[ "none" == $meson_provides ]]; then
+    meson_provides=$repo_name
 fi
 
-echo "     version: $version"
-echo " raw_version: $raw_version"
-echo "   repo_slug: $repo_slug"
-echo "   repo_name: $repo_name"
-echo "release_slug: $release_slug"
-echo "    provides: $provides"
+if [ 0 -lt $verbose ]; then
+    echo "       version: $version"
+    echo "   raw_version: $raw_version"
+    echo "     repo_slug: $repo_slug"
+    echo "     repo_name: $repo_name"
+    echo "  release_slug: $release_slug"
 
-echo ""
+    if [ 1 == $meson ]; then
+        echo "building meson wrap file"
+        echo "meson_provides: $meson_provides"
+    fi
+fi
+
 echo "Making the .tar.gz archive"
 git archive --format=tar.gz -o $release_slug.tar.gz --prefix=$release_slug/ $version
 
@@ -76,15 +82,17 @@ if [ 1 == $meson ]; then
     tgz_sum=`sha256sum $release_slug.tar.gz`
     tgz_sum=${tgz_sum:0:64} # Keep only the first 64 bytes which are the checksum
 
-    echo "[wrap-file]"                               > $repo_name.wrap
-    echo "directory = $release_slug"                >> $repo_name.wrap
-    echo ""                                         >> $repo_name.wrap
-    echo "source_filename = $release_slug.tar.gz"   >> $repo_name.wrap
-    echo "source_url = https://github.com/$repo_slug/releases/download/$version/$release_slug.tar.gz" >> $repo_name.wrap
-    echo "source_hash = $tgz_sum"                   >> $repo_name.wrap
-    echo ""                                         >> $repo_name.wrap
-    echo "[provides]"                               >> $repo_name.wrap
-    echo "lib$provides = lib${provides}_dep"        >> $repo_name.wrap
+    url="https://github.com/$repo_slug/releases/download/$version/$release_slug.tar.gz"
+
+    echo "[wrap-file]"                                    > $repo_name.wrap
+    echo "directory = $release_slug"                     >> $repo_name.wrap
+    echo ""                                              >> $repo_name.wrap
+    echo "source_filename = $release_slug.tar.gz"        >> $repo_name.wrap
+    echo "source_url = $url"                             >> $repo_name.wrap
+    echo "source_hash = $tgz_sum"                        >> $repo_name.wrap
+    echo ""                                              >> $repo_name.wrap
+    echo "[meson_provides]"                              >> $repo_name.wrap
+    echo "lib$meson_provides = lib${meson_provides}_dep" >> $repo_name.wrap
 
     echo "Making the sha256sums.txt file"
     sha256sum $release_slug.tar.gz $release_slug.zip $repo_name.wrap > $release_slug-sha256sums.txt
@@ -94,11 +102,33 @@ else
 fi
 
 echo "Copying files to the artifacts directory"
-mkdir artifacts
+mkdir -p artifacts
 cp ${release_slug}* artifacts/.
 
 if [ -f $repo_name.wrap ]; then
     cp $repo_name.wrap artifacts/.
 fi
 
-echo "Complete"
+output=0
+notes=''
+while read line; do
+if [[ "$line" =~ ^##.*\[$version\].*$ ]]; then
+    output=1
+elif [[ "$line" =~ ^##.*\[v?[0-9]+\.[0-9]+\.[0-9]+.*\].*$ ]]; then
+    output=0
+fi
+
+if [ 1 -eq $output ]; then
+    notes=$(echo "${notes}\\n$line")
+fi
+done <$changelog
+
+#cat $changelog | perl -0777 -ne 'print "$1\n"' # if /.*## \[${version}\]\s(.*?)\s+## \[(v\d+.\d+.\d+)\].*/s'
+
+#notes=$(cat $changelog | perl -0777 -ne 'print "$1\n" if /.*## \[${version}\]\s(.*?)\s+## \[(v\d+.\d+.\d+)\].*/s')
+today=`date +'%Y/%m/%d'`
+
+echo ::set-output name=release-name::$(echo ${version} ${today})
+echo ::set-output name=release-body::${notes}
+
+exit 0
