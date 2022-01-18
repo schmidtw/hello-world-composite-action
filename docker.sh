22cca783e1b854e3de5ce54f877c453db31db392
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2022-01-18 11:37:26 -0800

    Add a different composite action test.

---
+++ docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+env
+
+#docker run -it -d --name random-name --workdir $GITHUB_WORKSPACE
